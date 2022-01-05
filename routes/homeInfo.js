1da02e0810b92950897c3b9bb4fc8a7c40bdc152
Yulerik Sorensen <yulerik.sorensen@gmail.com>
2022-01-05 14:19:56 -0800

    moved routes for /bounty

--- routes/homeInfo.js
+++ routes/homeInfo.js
@@ -2,10 +2,16 @@ const express = require('express')
 const homeInfo = express.Router()
 const Bounty = require('../models/bounties')
 
+
 homeInfo.route('/')
-    .get((req, res) => {
-        console.log('route to /bounty for bounty list')
-        res.send('add endpoint /bounty to url parameter')
+    .get((req, res, next) => {
+        Bounty.find((error, bounty) => {
+            if (error) {
+                res.status(500)
+                return next(error)
+            }
+            return res.status(200).send(bounty)
+        })
     })
 homeInfo.get('/search', (req, res, next) => {
     const queryObj = req.query
@@ -18,5 +24,61 @@ homeInfo.get('/search', (req, res, next) => {
         return res.status(200).send(bounties)
     })
 })
+homeInfo.route('/bounty')
+    .get((req, res, next) => {
+        Bounty.find((error, bounty) => {
+            if (error) {
+                res.status(500)
+                return next(error)
+            }
+            return res.status(200).send(bounty)
+        })
+    })
+    .post((req, res, next) => {
+        const newBountyObj = new Bounty (req.body)
+        newBountyObj.save(err => {
+            if (err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(200).send(newBountyObj)
+        })
+    })
+homeInfo.route('/bounty/:bountyId')
+    .get((req, res, next) => {
+        Bounty.findById(req.params.bountyId, (err, bounty) => {
+            if (err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(200).send(bounty)            
+        })
+    })
+    .delete((req, res, next) => {
+        Bounty.findByIdAndRemove(req.params.bountyId, (error, bounty) => {
+            if (error) {
+                res.status(500)
+                return next(error)
+            }
+            const response = {
+                message: 'Bounty successfully deleted'
+            }
+            return res.status(200).send(response)
+        })
+    })
+    .put((req, res, next) => {
+        Bounty.findByIdAndUpdate(
+            req.params.bountyId, 
+            req.body, 
+            {new:true},
+            (err, bounty) => {
+                if (err) {
+                    res.status(500)
+                    return next(err)
+                }
+                return res.send(bounty)
+            }
+        )
+    })
 
 module.exports = homeInfo
--- server.js
+++ server.js
@@ -5,49 +5,38 @@ const mongoose = require('mongoose')
 const app = express()
 const port = process.env.PORT || 9000
 require("dotenv").config()
-
 const uri = process.env.MONGODB_URI || "mongodb+srv://forrest:Juliette92@cluster0.ab21w.mongodb.net/bounty-hunter?retryWrites=true&w=majority"
+
+// ... other imports 
+const path = require("path")
 
 // every request (middleware)
 app.use(express.json())
 app.use(morgan('dev'))
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 // connect to database
 mongoose.connect(uri, () => console.log('connected thru mongodb'))
 // mongoose.connect("mongodb+srv://forrest:Juliette92@cluster0.ab21w.mongodb.net/bounty-hunter?retryWrites=true&w=majority", () => console.log('remote db connection'));
-
-// mongoose.connect('mongodb://localhost:27017/bounties', 
-// // {
-// //     useNewUrlParser: true,
-// //     // useUnitfiedTopology: true,
-// //     useCreateIndex: true,
-// //     useFindAndModify: false
-// // },
+// mongoose.connect('mongodb://localhost:27017/bounties',
 // () => console.log('Connected to the database.') 
 // )
+
 // different routes
 app.use('/', require('./routes/homeInfo'))
-app.use('/bounty', require('./routes/bounty'))
+// app.use('/bounty', require('./routes/bounty'))
 // error handling
 app.use((err, req, res, next) => {
     console.log(err)
     return res.send({errMsg: err.message})
 })
 
-// ... other imports 
-const path = require("path")
-
-// ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
-
 // ...
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-
-
-
-
 
 app.listen(port, () => {
     console.log(`Running on ${port}`)
