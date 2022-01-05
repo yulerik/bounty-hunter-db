5f594000a95beceaa9d4a857fb2238639d46983c
Yulerik Sorensen <yulerik.sorensen@gmail.com>
2022-01-04 20:16:59 -0800

    changed db route to remote server instead of local.

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ const port = process.env.PORT || 9000
 app.use(express.json())
 app.use(morgan('dev'))
 // connect to database
-mongoose.connect('mongodb://localhost:27017/bounties', 
-// {
-//     useNewUrlParser: true,
-//     // useUnitfiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false
-// },
-() => console.log('Connected to the database.') 
-)
+
+mongoose.connect("mongodb+srv://forrest:Juliette92@cluster0.ab21w.mongodb.net/bounty-hunter?retryWrites=true&w=majority", () => console.log('remote db connection'));
+
+// mongoose.connect('mongodb://localhost:27017/bounties', 
+// // {
+// //     useNewUrlParser: true,
+// //     // useUnitfiedTopology: true,
+// //     useCreateIndex: true,
+// //     useFindAndModify: false
+// // },
+// () => console.log('Connected to the database.') 
+// )
 // different routes
 app.use('/', require('./routes/homeInfo'))
 app.use('/bounty', require('./routes/bounty'))
@@ -26,6 +29,22 @@ app.use((err, req, res, next) => {
     console.log(err)
     return res.send({errMsg: err.message})
 })
+
+// // ... other imports 
+// const path = require("path")
+
+// // ... other app.use middleware 
+// app.use(express.static(path.join(__dirname, "client", "build")))
+
+// // ...
+// // Right before your app.listen(), add this:
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Running on ${port}`)
